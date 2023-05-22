21bbce16b6c47268262c880781257b7bc859f389
Belletre Jules <belletreju@pc-cy301-016.cpi.cergy.eisti.fr>
2023-05-22 15:41:58 +0200

    i am the one who knocks

--- data.php
+++ data.php
@@ -1,16 +1,30 @@
 <?php
+$mail = $_POST["email"];
+$row = 1;
+if (($handle = fopen('BDD/'.$mail[0].'.csv', "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num champs à la ligne $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
+
+
 $password = $_POST["password"];
 echo hash('sha256', '962Crystal');
 $hasedPassword = hash('sha256', $password);
 $fname = $_POST["fname"];
 $name = $_POST["name"];
 $birthdate = $_POST["birthday"];
-$mail = $_POST["email"];
 $list = array (
    array($fname, $name, $birthdate, $mail, $hasedPassword)
 );
 
-$fp = fopen('BDD/'.$mail[0].'.csv', 'w');
+$fp = fopen('BDD/'.$mail[0].'.csv', 'a+');
 foreach ($list as $fields) {
     fputcsv($fp, $fields);
 }
