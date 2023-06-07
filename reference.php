1479b2d4c41fcb71aece5c808ec94db96d304519
Belletre Jules <belletreju@pc-cy302-033.cpi.cergy.eisti.fr>
2023-06-07 12:19:42 +0200

    added id gestion

--- reference.php
+++ reference.php
@@ -3,6 +3,7 @@ $handle = fopen('BDD2/'.reference.'.csv', "r+");
 $id = fgetcsv($handle, 1000, ",");
 echo $id[0];
 $id[0]++;
-fputcsv($handle, $id[0]);
+file_put_contents('BDD2/'.reference.'.csv', implode(PHP_EOL, $id));
+fclose ($handle);
 echo $_POST["exp"]
-?>  
+?>      
