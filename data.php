8a17004b36dca91314ad9d85714c42b0156de731
Jules Bellêtre <julesbelletre@macbook-pro.home>
2023-06-07 23:30:19 +0200

    worked on 2nd database

--- data.php
+++ data.php
@@ -1,70 +1,68 @@
 <?php
 session_start();
-$_SESSION['name']=$_POST["name"];
-$_SESSION['fname']=$_POST["fname"];
-$_SESSION['email']=$_POST["email"];
-$_SESSION['birthday']=$_POST["birthday"];
-$_SESSION['password']=$_POST["password"];
+$_SESSION['name'] = $_POST["name"];
+$_SESSION['fname'] = $_POST["fname"];
+$_SESSION['email'] = $_POST["email"];
+$_SESSION['birthday'] = $_POST["birthday"];
+$_SESSION['password'] = $_POST["password"];
 echo $_SESSION['name'];
 
 $mail = $_POST["email"];
 $row = 1;
 $test = 1;
-if ($mail !== "" && $_POST["password"] !== "" && $_POST["name"] !== "" && $_POST["fname"] !== "" && $_POST["birthday"] !== ""){
-    if (($handle = fopen('BDD/'.$mail[0].'.csv', "r")) !== FALSE) {
+if ($mail !== "" && $_POST["password"] !== "" && $_POST["name"] !== "" && $_POST["fname"] !== "" && $_POST["birthday"] !== "") {
+    if (($handle = fopen('BDD/' . $mail[0] . '.csv', "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE || $data != "") {
-            if (isset($data) && count($data) > 3 && $mail == $data[3]){
-                echo "compte déjà existant" ;
+            if (isset($data) && count($data) > 3 && $mail == $data[3]) {
+                echo "compte déjà existant";
                 $test = 0;
             }
         }
     }
-}
-else{
+} else {
     echo "Tous les champs ne sont pas valides";
     $test = 0;
 }
 
-if ($test == 1){
-    
+if ($test == 1) {
+
     //récupere l'id
-    $handle = fopen('BDD/'.$mail[0].'.csv', "a+");
+    $handle = fopen('BDD/' . $mail[0] . '.csv', "a+");
     $line = fgetcsv($handle);
-    if($line != ""){
-    $firstvalue = $line[0];
-    $id  =intval($firstvalue) + 1 ;
-    $_SESSION['ID'] = $id;
-    echo "\n";
-    echo $_SESSION['ID'];
-    fclose($handle);
-    echo 'val is'. $id;
-    $handle = fopen('BDD/'.$mail[0].'.csv', "c+");
-    fseek($handle, 0);
-    fwrite($handle, $id. PHP_EOL);
-    fclose($handle);
+    if ($line != "") {
+        $firstvalue = $line[0];
+        $id = intval($firstvalue) + 1;
+        $_SESSION['ID'] = $id;
+        echo "\n";
+        echo $_SESSION['ID'];
+        fclose($handle);
+        echo 'val is' . $id;
+        $handle = fopen('BDD/' . $mail[0] . '.csv', "c+");
+        fseek($handle, 0);
+        fwrite($handle, $id . PHP_EOL);
+        fclose($handle);
     }
 
     // cas ou le fichier est initialement vide
-
-    else{
-        fwrite($handle, '1'. PHP_EOL);
-        $id = 1 ;
+    else {
+        fwrite($handle, '1' . PHP_EOL);
+        $id = 1;
         fclose($handle);
     }
-    
+
     $password = $_POST["password"];
     echo hash('sha256', '962Crystal');
     $hasedPassword = hash('sha256', $password);
-    $_SESSION['hidden_password']= $hasedPassword;
+    $_SESSION['hidden_password'] = $hasedPassword;
     echo $_SESSION['hidden_password'];
     $fname = $_POST["fname"];
     $name = $_POST["name"];
     $birthdate = $_POST["birthday"];
-    $list = array (
-    array($fname, $name, $birthdate, $mail, $hasedPassword)
+    $list = array(
+        array($fname, $name, $birthdate, $mail, $hasedPassword)
     );
-    $fp = fopen('BDD/'.$mail[0].'.csv', 'a+');
-    fwrite($fp, $id.',');
+    $fp = fopen('BDD/' . $mail[0] . '.csv', 'a+');
+    fwrite($fp, $id . ',');
     foreach ($list as $fields) {
         fputcsv($fp, $fields);
     }
--- reference.php
+++ reference.php
@@ -3,7 +3,23 @@ $handle = fopen('BDD2/reference.csv', "a+");
 $id = fgetcsv($handle, 1000, ",");
 echo $id[0];
 $id[0]++;
-file_put_contents('BDD2/reference.csv', implode($id[0].PHP_EOL, $id));
-fclose ($handle);
-echo $_POST["exp"]
-?>      
+fclose($handle);
+$handle = fopen('BDD2/reference.csv', "cÒ+");
+fseek($handle, 0);
+fwrite($handle, $id[0] . PHP_EOL);
+fclose($handle);
+echo $_POST["exp"];
+$exp = $_POST["exp"];
+$name = $_POST["name"];
+$fname = $_POST["fname"];
+$mail = $_POST["mailref"];
+$list = array(
+    array($exp, $name, $fname, $mail)
+);
+$fp = fopen('BDD2/reference.csv', 'a+');
+fwrite($fp, $id[0] . ',');
+foreach ($list as $fields) {
+    fputcsv($fp, $fields);
+}
+fclose($fp);
+?>
