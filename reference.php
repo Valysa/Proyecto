208cf32219d330b82fcfca14da424d3592b2c1d1
Valysa <104635175+Valysa@users.noreply.github.com>
2023-06-07 21:32:56 +0200

    ça marchait pas maintenant ça marche

--- reference.php
+++ reference.php
@@ -1,9 +1,9 @@
 <?php
-$handle = fopen('BDD2/'.reference.'.csv', "r+");
+$handle = fopen('BDD2/reference.csv', "r+");
 $id = fgetcsv($handle, 1000, ",");
 echo $id[0];
 $id[0]++;
-file_put_contents('BDD2/'.reference.'.csv', implode(PHP_EOL, $id));
+file_put_contents('BDD2/reference.csv', implode($id[0].PHP_EOL, $id));
 fclose ($handle);
 echo $_POST["exp"]
 ?>      
