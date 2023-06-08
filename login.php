f7f4fb8e9fc85ef9fd828636a0188e079db8f904
Valysa <104635175+Valysa@users.noreply.github.com>
2023-06-08 12:24:38 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,24 @@
 <?php
 $mail = $_POST["email"];
-$password = hash('sha256', $_POST["password"]);
 $row = 1;
+$test = 0;
 if (($handle = fopen('BDD/'.$mail[0].'.csv', "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        if ($mail == $data[3]){
-            if ($password == $data[4]){
-                echo $data[3];
+        if ($mail == $data[4]){
+            $test = 1;
+            $salt = $data[2].$data[1];
+            $password = hash('sha256', $salt.$_POST["password"]);
+            echo $password;
+            if ($password == $data[5]){
+                echo $data[4];
             }
             else{
                 echo "le mot de passe est pas bon chacal";
             }
         }
+    }
+    if($test == 0){
+        echo "le mail n'existe sensiblement pas dans la base de donnée";
     }
     fclose($handle);
 }
