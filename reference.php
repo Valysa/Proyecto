bea2dfd7925b49084024578ede9686094e12d28e
Valysa <104635175+Valysa@users.noreply.github.com>
2023-06-08 17:43:18 +0200

    Update reference.php

--- reference.php
+++ reference.php
@@ -1,29 +1,32 @@
 <?php
     session_start();
-    echo $_SESSION['ID']; 
-    $handle = fopen('BDD2/reference.csv', "a+");
-    $id = fgetcsv($handle, 1000, ",");
-    echo $id[0];
-    $id[0]++;
-    fclose($handle);
-    $handle = fopen('BDD2/reference.csv', "cÒ+");
-    fseek($handle, 0);
-    fwrite($handle, $id[0] . PHP_EOL);
-    fclose($handle);
-    echo $_POST["exp"];
-    $exp = $_POST["exp"];
-    $name = $_POST["name"];
-    $fname = $_POST["fname"];
-    $mail = $_POST["mailref"];
-    $list = array(
-        array($exp, $name, $fname, $mail)
-    );
-    $fp = fopen('BDD2/reference.csv', 'a+');
-    fwrite($fp, $id[0] . ',');
-    foreach ($list as $fields) {
-        fputcsv($fp, $fields);
-    }
-    fclose($fp);
+echo $_SESSION['ID']; 
+$handle = fopen('BDD2/reference.csv', "a+");
+$id = fgetcsv($handle, 1000, ",");
+echo $id[0];
+$id[0]++;
+fclose($handle);
+$handle = fopen('BDD2/reference.csv', "cÒ+");
+fseek($handle, 0);
+fwrite($handle, $id[0] . PHP_EOL);
+fclose($handle);
+echo $_POST["exp"];
+// liste des champs à rentrer dans la database
+$exp = $_POST["exp"];
+$name = $_POST["name"];
+$fname = $_POST["fname"];
+$mail = $_POST["mailref"];
+$list = array(
+    array($exp, $name, $fname, $mail, $_SESSION['ID'])
+);
+// insertion des champs dans BDD2
+$fp = fopen('BDD2/reference.csv', 'a+');
+fwrite($fp, $id[0] . ',');
+foreach ($list as $fields) {
+    fputcsv($fp, $fields);
+}
+fclose($fp);
+// double chainage (insertion de l'id de la référence dans la table du jeune)
 
     //part to sent an email to the referent
     use PHPMailer\PHPMailer\PHPMailer;
