34f5f399ce18cc15a32c3335b57423ead09c9980
Valysa <104635175+Valysa@users.noreply.github.com>
2023-06-09 18:26:12 +0200

    Update consult.php

--- consult.php
+++ consult.php
@@ -23,31 +23,65 @@
                 $data[] = $row;
             }
             fclose($file);
+            $i = 1;
+            echo '<table>';
             foreach ($data as $row) {
                 if ($row[5] == $_SESSION["ID"]) {
-                    $info1 = $row[3]; // Première colonne du CSV
-                    $info2 = $row[2]; // Deuxième colonne du CSV
-                    $info3 = $row[1]; // Troisième colonne du CSV
-                    if($row[6] == "validated"){
-                        echo '<div class="row">';
-                        echo '<div class="styled-box">';
-                        echo '<input type="checkbox" name="selection[]" value="' . $row[0] . '">';
-                        echo '<p>' . $info1 . '</p>';
-                        echo '<p>' . $info2 . '</p>';
-                        echo '<p>' . $info3 . '</p>';
-                        echo '</div>';
+                    $i++;
+                    if ($i % 2 == 0) {
+                        $info1 = $row[3]; // Première colonne du CSV
+                        $info2 = $row[2]; // Deuxième colonne du CSV
+                        $info3 = $row[1]; // Troisième colonne du CSV
+                        if ($row[6] == "validated") {
+                            echo '<tr><td>';
+                            echo '<div class="styled-box">';
+                            echo '<input type="checkbox" name="selection[]" value="' . $row[0] . '">';
+                            echo '<p>' . $info1 . '</p>';
+                            echo '<p>' . $info2 . '</p>';
+                            echo '<p>' . $info3 . '</p>';
+                            echo '</div>';
+                            echo '</td>' ;
+                        } else {
+                            echo '<tr><td>';
+                            echo '<div class="styled-box">';
+                            echo 'cette reference est en attente de validation';
+                            echo '<p>' . $info1 . '</p>';
+                            echo '<p>' . $info2 . '</p>';
+                            echo '<p>' . $info3 . '</p>';
+                            echo '</div>';
+                            echo '</td>' ;
+                        }
                     }
                     else{
-                        echo '<div class="row">';
-                        echo '<div class="styled-box">';
-                        echo 'Cette référence est en attente de validation';
-                        echo '<p>' . $info1 . '</p>';
-                        echo '<p>' . $info2 . '</p>';
-                        echo '<p>' . $info3 . '</p>';
-                        echo '</div>';
+                        $info1 = $row[3]; // Première colonne du CSV
+                        $info2 = $row[2]; // Deuxième colonne du CSV
+                        $info3 = $row[1]; // Troisième colonne du CSV
+                        if ($row[6] == "validated") {
+                            echo '<td>';
+                            echo '<div class="styled-box">';
+                            echo '<input type="checkbox" name="selection[]" value="' . $row[0] . '">';
+                            echo '<p>' . $info1 . '</p>';
+                            echo '<p>' . $info2 . '</p>';
+                            echo '<p>' . $info3 . '</p>';
+                            echo '</div>';
+                            echo '</td></tr>' ;
+                        } else {
+                            echo '<td>';
+                            echo '<div class="styled-box">';
+                            echo 'cette reference est en attente de validation';
+                            echo '<p>' . $info1 . '</p>';
+                            echo '<p>' . $info2 . '</p>';
+                            echo '<p>' . $info3 . '</p>';
+                            echo '</div>';
+                            echo '</td></tr>' ;
+                        }
                     }
                 }
             }
+            if($i%2 == 0){
+                echo'</tr>';
+            }
+            echo '</table>';
             ?>
             <div class="row">
                 <input type="submit" value="Valider" style="float: right;">
