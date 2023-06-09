dbaac22809f89f174c056b838cd195c5edf92313
Guyot Julien <guyotjulie@pc-cy303-013.cpi.cergy.eisti.fr>
2023-06-09 16:26:24 +0200

    update

--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     session_unset();
     echo session_status();
     session_destroy();
--- monEspace.php
+++ monEspace.php
@@ -1,5 +1,20 @@
 <?php
-    session_start();
+    function is_session_started()
+    {
+        if ( php_sapi_name() !== 'cli' ) {
+            if ( version_compare(phpversion(), '5.4.0', '>=') ) {
+                return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+            } else {
+                return session_id() === '' ? FALSE : TRUE;
+            }
+        }
+        return FALSE;
+    }
+    
+    // Example
+    if ( is_session_started() === FALSE ) session_start();
+
+    echo session_status();
 ?>
 <!DOCTYPE html>
 <html>
