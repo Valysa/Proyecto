b54557a557c1e196ad6ed97d31dc2448d9b0f0ac
jeffdebruh <105702766+jeffdebruh@users.noreply.github.com>
2023-06-10 18:24:34 +0200

    Create checked.php

---
+++ checked.php
@@ -0,0 +1,13 @@
+<?php
+
+$skill= $_POST['skill'];
+    $a = array(0,0,0,0,0,0,0,0,0);
+
+    foreach ($skill as $b){
+        echo $b;
+        $a[$b]=1;
+    }
+    foreach ($a as $c){
+        echo $c."<br />";
+    }
+?>
