aaa9c48a5f0d9cfc3508b7559ca92f84e65aa66d
Jteam1 <guyotjulie@cy-tech.fr>
2023-06-11 18:49:36 +0200

    update

--- refID.php
+++ refID.php
@@ -80,10 +80,12 @@
         <form id="info" class="main_input" action="validateReference.php?ref=<?php echo $_GET['ref']; ?>" method="POST">
                 <table id="infos" style="width: 100%;">
                     <tr>
+                    <form id=comment_area action="validateReference.php?ref=<?php echo $_GET['ref']; ?>" method="POST" >
                         <td style="width: 25%;">
                                 <label for="comments">COMMENTAIRE</label> <br>
                                 <textarea type="text" name="comments"></textarea>
                         </td>
+                    </form>
                         <td>
                             <fieldset>
                                 <div class="input_text">
